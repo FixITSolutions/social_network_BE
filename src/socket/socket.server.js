a57317e1b480a9d617ec774a432a6e54622feccf
Rares-FixIT <tech.solutions.fixit@gmail.com>
2022-11-17 16:02:48 +0200

    Fully Functional Commit
    Updated dependencies
    Removed Previous Profile Pictures
    Fixed Socked.io connection issues

--- src/socket/socket.server.js
+++ src/socket/socket.server.js
@@ -8,16 +8,19 @@ class SocketServer {
     }
 
     initialize(server) {
-        this.server = io.listen(server);
+        this.server = io(server, {
+            cors: {
+                origin: 'http://localhost:4200',
+                methods: ["GET", "POST"]
+            }
+        });
         this.server.on('connection', (socket) => {
-            // console.log(socket.handshake.address)
             console.log('A new user has entered the chat')
             this.bindEvents(socket);
-            socket.on('disconnect', () => {
-                console.log('disconnected my man')
-                    //crash happens with this shit
-                    // removeUserFromOnline(this.currentUser.id);
-            });
+            // socket.on('disconnect', () => {
+            //     console.log('disconnected my man')
+            //     this.removeUserFromOnline(this.currentUser.id);
+            // });
         })
     }
 
@@ -26,14 +29,16 @@ class SocketServer {
             socket.broadcast.emit('messageFromServer', data);
         })
         socket.on('userDetails', (data) => {
-            // console.log(data)
             this.currentUser = data;
-            console.log(this.currentUser);
             const check = this.checkIfUserOnline(data.id);
             if (check) {
                 this.onlineUsers.push({ id: data.id, name: data.name })
             }
             socket.emit('usersOnline', this.onlineUsers)
+        })
+        socket.on('removeUser', (data) => {
+            this.currentUser = data;
+            this.removeUserFromOnline(data.id);
         })
     }
 
@@ -47,10 +52,12 @@ class SocketServer {
     }
 
     removeUserFromOnline(userID) {
+        if (!userID || userID !== this.currentUser.id) {
+            return;
+        }
         for (let i = 0; i < this.onlineUsers.length; i++) {
             if (this.onlineUsers[i].id === userID) {
                 this.onlineUsers.splice(i, 1);
-                console.log(this.onlineUsers);
             }
         }
     }
